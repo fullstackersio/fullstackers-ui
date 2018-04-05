67e7a54b7482f43a168cf50570058616fd609bab
Mark McCollum <mmccollum@centricgroup.com>
2018-04-05 13:57:08 -0500

    converted over to Angular 5

---
+++ src/app/presentation/presentation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fsio-presentation',
+  templateUrl: './presentation.component.html',
+  styleUrls: ['./presentation.component.scss']
+})
+export class PresentationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/presentations/presentations.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fsio-presentations',
+  templateUrl: './presentations.component.html',
+  styleUrls: ['./presentations.component.scss']
+})
+export class PresentationsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

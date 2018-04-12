535a97e2634289e4db872c244e835748bef42440
chris-schmitz <schmitz.chris@gmail.com>
2018-04-11 21:38:12 -0500

    WIP: starting to make presentations dynamically loaded

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core"
+import { Routes, RouterModule } from "@angular/router"
+import { PresentationsComponent } from "./presentations/presentations.component"
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "presentations",
+    component: PresentationsComponent
+  }
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
